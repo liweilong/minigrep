00ce4e8c6545ceabde23cd863838a6d12f25da0b
李伟龙 <liweilong.jerry@bytedance.com>
2020-12-14 08:47:04 +0800

    closure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
 use std::env;
 use std::process;
+use std::thread;
+use std::time::Duration;
 
 use minigrep;
 use minigrep::Config;
 
 
 fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+
+    generate_workout(
+        simulated_user_specified_value,
+         simulated_random_number,
+    );
+
+}
+
+
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    if intensity < 25 {
+        let expensive_result = expensive_closure(intensity);
+        println!(
+            "Today, do {} pushups!",
+            expensive_result
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
+        }
+    }
+}
+
+fn grep() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
@@ -19,7 +60,6 @@ fn main() {
         process::exit(1);
     }
 }
-
 
 
 
